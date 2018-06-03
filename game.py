a6879bdb9f745bae68a0970ee1278766da4b0f6e
Ken Naishtat <kenners@gmail.com>
2018-06-02 19:32:49 -0700

    Woop Woop

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
 """Wine Jam 2018 @kenners"""
 import cocos
+from cocos.actions import Delay, Repeat, Reverse, ScaleBy
 
-class Start(cocos.layer.Layer):
+class Start(cocos.layer.ColorLayer):
+
+    """Our hero"""
+    hero = None
+
     """Starter layer"""
     def create_label(self, text, xpos, ypos):
         """Create a label on this layer."""
@@ -14,15 +19,27 @@ class Start(cocos.layer.Layer):
         label.position = xpos, ypos
         self.add(label)
 
+    def create_sprite(self, asset_path, coords, scale=1, z_layer=1):
+        """Create a sprite on this layer."""
+        sprite = cocos.sprite.Sprite(asset_path)
+        sprite.position = coords
+        sprite.scale = scale
+        self.hero = sprite
+        self.add(sprite, z=z_layer)
+
+
 def main():
     """Main entry point."""
     # Creates the window
     cocos.director.director.init()
 
     # Creates the layer with some text on it
-    l_start = Start()
+    l_start = Start(64, 64, 224, 255) # rgba
     l_start.create_label("I got the strap", 320, 240)
     l_start.create_label("I gotta carry 'em", 320, 200)
+    l_start.create_sprite('gambino.jpg', (320, 375), scale=0.75)
+    bounce = ScaleBy(2, duration=1)
+    l_start.hero.do(Delay(2) + Repeat(bounce + Reverse(bounce)))
 
     # Creates a scene with the layer on it
     main_scene = cocos.scene.Scene(l_start)
